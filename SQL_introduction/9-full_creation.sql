dc5af4c924451a004b1c500ca37d328b1b2c4049
Remenyr <puertaalejo02@gmail.com>
2023-03-16 20:25:17 -0500

    Full creation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -6,7 +6,4 @@ CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
 );
 
 INSERT INTO hbtn_0c_0.second_table (id, name, score) 
-VALUES  (1, "John", 10),
-        (2, "Alex", 3),
-        (3, "Bob", 14),
-        (4, "George", 8);
+VALUES  (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
