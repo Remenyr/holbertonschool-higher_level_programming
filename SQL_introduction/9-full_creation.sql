5e759f8a0c7a240af0811e3bff4cc8b0f6970a3c
Remenyr <puertaalejo02@gmail.com>
2023-03-16 20:26:15 -0500

    Full creation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,9 +1,9 @@
 -- This is a script that creates a table second_table in the database hbtn_0c_0 in my MySQL server and add multiples rows.
-CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
+CREATE TABLE IF NOT EXISTS second_table (
     id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
     score INT NOT NULL
 );
 
-INSERT INTO hbtn_0c_0.second_table (id, name, score) 
+INSERT INTO second_table (id, name, score) 
 VALUES  (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
