494705cafaf3aacf6dcf96de54d30976784d40a2
Remenyr <puertaalejo02@gmail.com>
2023-03-17 15:19:06 -0500

    Get all states

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Script that list rows of a database """
+from sys import argv
+import MySQLdb
+
+
+if __name__ == "__main__":
+    """script that lists all states from the database hbtn_0e_0_usa"""
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=argv[1], passwd=argv[2], database=argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+
+    cur.close()
+    conn.close()
