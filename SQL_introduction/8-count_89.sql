ad7c103d006eeb3ef76c28a42a49c4b27dbfe525
Remenyr <puertaalejo02@gmail.com>
2023-03-16 20:19:43 -0500

    Count 89

---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,2 @@
+-- This is a script that displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in my MySQL server.
+SELECT COUNT(*) FROM first_table WHERE id = 89;
