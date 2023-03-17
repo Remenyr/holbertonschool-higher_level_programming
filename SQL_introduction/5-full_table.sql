020c922c9c4e60ec1434c85957632af492935364
Remenyr <puertaalejo02@gmail.com>
2023-03-16 19:43:03 -0500

    Full description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,2 @@
 -- This is a script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
-SELECT column_name, column_type
-FROM information_schema.columns
-WHERE table_name = 'first_table'
-AND table_schema = 'hbtn_0c_0';
+SHOW CREATE TABLE first_table;
